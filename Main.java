762091d7cf4aacc4dd0771384bc82e5d484bf755
Mikolaj <mikolaj.rych@gmail.com>
2018-04-23 14:05:18 +0200

    Test

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class Main{
     public static void main(String[] args) {
         // Game game = new Game();
         // game.startNewGame();
+        Deck deck = new Deck();
+        for(Card card : deck.getListOfCards()) {
+            System.out.println(card.toString());
+        }
     }
 }
