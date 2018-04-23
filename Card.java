99a7074bba37fcc8d0e21675be8a9d0ba780c1fa
polon59 <przemyslaw.polczak@gmail.com>
2018-04-23 21:48:52 +0200

    implement color display in 'toString' method

--- Card.java
+++ Card.java
@@ -10,7 +10,19 @@ public class Card {
     }
     
     public String toString() {
-        String cardString = "Suit" + suit + "Rank" + rank;
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        String cardString;
+        String cardASCII;
+
+        if (suit == 1 || suit == 2){
+            cardString = ANSI_RED + "Suit" + suit + "Rank" + rank + ANSI_RESET;
+        }
+        else{
+            cardString = ANSI_BLACK + "Suit" + suit + "Rank" + rank + ANSI_RESET;
+        }
+
         return cardString;
     }
 }
