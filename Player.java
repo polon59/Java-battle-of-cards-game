0e1834a9fd9f1feb7b5a10bf1a216d7536ed55bd
Mikolaj <mikolaj.rych@gmail.com>
2018-04-23 14:40:22 +0200

    Define drawCards method

--- Player.java
+++ Player.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // abstract class for class Player
 public abstract class Player{
-
+    protected List<Card> cardsInHand;
 
 public abstract Card pickCard();
 
-
+public List<Card> drawCards(List<Card> list) {
+    cardsInHand = new ArrayList<>();
+    for (int i = 0; i < 9; i++) {
+        cardsInHand.add(list.get(i));
+        list.remove(i);
+    }
+    return cardsInHand;
+}
 
 
 
