6eab13ff5168cca2f7b9d138325e2f6e96357b72
Mikolaj <mikolaj.rych@gmail.com>
2018-04-23 14:04:29 +0200

    Define Card class

--- Card.java
+++ Card.java
@@ -1,5 +1,16 @@
-
 public class Card {
+    int rank;
+    int suit;
+    boolean faceDown;
 
+    public Card(int rank, int suit, boolean faceDown) {
+        this.rank = rank;
+        this.suit = suit;
+        this.faceDown = faceDown;
+    }
     
+    public String toString() {
+        String cardString = "Suit" + suit + "Rank" + rank;
+        return cardString;
+    }
 }
