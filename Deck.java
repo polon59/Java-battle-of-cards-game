fad13936417222efffef64f84f5fa7775f2d1e6c
Mikolaj <mikolaj.rych@gmail.com>
2018-04-23 14:06:30 +0200

    Implement shuffle and construct methods

--- Deck.java
+++ Deck.java
@@ -1,3 +1,27 @@
-public class Deck{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    List<Card> listOfCards;
+
+    public Deck() {
+        listOfCards = new ArrayList<>();
+        for (int suit = 1; suit < 5; suit++) {
+            for (int rank = 1; rank < 14; rank++) {
+                listOfCards.add(new Card(rank, suit, true));
+            }
+        }
+        shuffle();
+    }
+
+    public List<Card> getListOfCards(){ 
+        return listOfCards;
+    }
+
+    public void shuffle() {
+        Collections.shuffle(listOfCards);
+    }
+
     
 }
