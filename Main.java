f970756d47444999a3db0299050f5e1a86b3a6b3
polon59 <przemyslaw.polczak@gmail.com>
2018-04-23 21:54:26 +0200

    add temporary methods for testing card ASCII display

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Main{
 
     public static void main(String[] args) {
@@ -8,5 +10,11 @@ public class Main{
         Player computer = new Computer(deck);
         System.out.println(person.cardsInHand.toString());
         System.out.println(computer.cardsInHand.toString());
+
+        List<Card> cards = deck.getListOfCards();
+        System.out.println(cards.size());
+        for (int i = 0; i<cards.size(); i++){
+            System.out.println(cards.get(i).toString());
+        }
     }
 }
