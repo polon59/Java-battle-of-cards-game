a4044e0a7566a9fd4d3bfd740bba02b5068b32b0
Mikolaj <mikolaj.rych@gmail.com>
2018-04-25 16:51:36 +0200

    clean code

--- Player.java
+++ Player.java
@@ -10,13 +10,15 @@ public abstract class Player {
 
     public abstract int chooseOption(Deck deck, Card patternCard);
 
+    public abstract void addCardsFromPile(Deck pileOnTable);
+
     public void move(Player opponent, Deck deck, Card patternCard) {
         int option = chooseOption(deck, patternCard);
         if (option == 1) {
             placeCardOnTop(deck, patternCard);
-        } else { 
+        } else {
             check(deck, opponent, patternCard);
-        } 
+        }
     }
 
     public void placeCardOnTop(Deck deck, Card patternCard) {
@@ -24,7 +26,7 @@ public abstract class Player {
         Card chosenCard = getCardsInHand().get(index);
         chosenCard.setFaceDown(true);
         deck.addCardToPile(chosenCard);
-        cardsInHand.remove(index);      
+        cardsInHand.remove(index);
     }
 
     public void drawCards(Deck deck) {
@@ -38,19 +40,20 @@ public abstract class Player {
 
     }
 
-    public abstract void addCardsFromPile(Deck pileOnTable);
-    
     public void check(Deck pileOnTable, Player opponent, Card patternCard) {
         if (pileOnTable.getSizeOfPile() == 0) {
             System.out.println("Can't check your opponent's move, there hasn't been");
             return;
-        }
-        if (pileOnTable.isCardValid(patternCard)) {
-            this.addCardsFromPile(pileOnTable);
-            pileOnTable.clearPile();
         } else {
-            opponent.addCardsFromPile(pileOnTable);
-            pileOnTable.clearPile();
+            if (pileOnTable.isCardValid(patternCard)) {
+                if (this.cardsInHand.size() + pileOnTable.getSizeOfPile() < 18) {
+                    this.addCardsFromPile(pileOnTable);
+                    pileOnTable.clearPile();
+                }
+            } else {
+                opponent.addCardsFromPile(pileOnTable);
+                pileOnTable.clearPile();
+            }
         }
     }
 
@@ -66,7 +69,11 @@ public abstract class Player {
         final String ANSI_BLUE = "\u001B[34m";
 
         for (int cardIndex = 0; cardIndex < cardsInHand.size(); cardIndex++) {
-            System.out.print(cardIndex + 1 + "       ");
+            if (cardIndex < 10) {
+                System.out.print(cardIndex + 1 + "       ");
+            } else {
+                System.out.print(cardIndex + 1 + "      ");
+            }
         }
 
         System.out.println();
@@ -74,9 +81,6 @@ public abstract class Player {
         for (int currentLineNumber = 0; currentLineNumber < allLines; currentLineNumber++) {
 
             for (int i = 0; i < cardsInHand.size(); i++) {
-                //cardLineToPrint = "";
-
-                // change to getFaceDown
                 if (cardsInHand.get(i).faceDown) {
                     System.out.print(ANSI_BLUE + cardsInHand.get(i).readASCIIfromFile().get(currentLineNumber) + " "
                             + ANSI_RESET);
