3b3ece1d5cf68c05e8c98add4f32e472a1983782
Mikolaj <mikolaj.rych@gmail.com>
2018-04-26 14:16:15 +0200

    implement AI advance logic

--- Computer.java
+++ Computer.java
@@ -4,6 +4,7 @@ import java.util.Random;
 public class Computer extends Player {
     private Random generator = new Random();
     private Card validCard;
+    private List<Card> validCards;
 
     public Computer(Deck deck) {
         drawCards(deck);
@@ -13,13 +14,19 @@ public class Computer extends Player {
     }
 
     public int pickCard(Deck deck, Card patternCard) {
-        
+
         int cardIndex;
-        if (deck.getSizeOfPile() <= 12) {
-            cardIndex = generator.nextInt(cardsInHand.size());
-        } else {
+        if (deck.getSizeOfPile() > 12) {
+            if (haveValidCard(patternCard) && validCards.size() > 1
+                    || cardsInHand.size() == 1 && haveValidCard(patternCard)) {
                 cardIndex = cardsInHand.indexOf(validCard);
+            } else {
+                cardIndex = generator.nextInt(cardsInHand.size());
+            }
+        } else {
+            cardIndex = generator.nextInt(cardsInHand.size());
         }
+
         return cardIndex;
     }
 
@@ -32,22 +39,21 @@ public class Computer extends Player {
 
     public int chooseOption(Deck deck, Card patternCard, Player opponent) {
         int option = 0;
-        if(isLastComputerCard == true){
+        if (isLastComputerCard == true) {
             option = 1;
-        }
-        else if (cardsInHand.size() + deck.getSizeOfPile() == 18) {
+        } else if (opponent.cardsInHand.size() == 0) {
             option = 2;
-        } 
-        else if (cardsInHand.size() == 1) {
+        } else if (cardsInHand.size() == 1) {
             if (haveValidCard(patternCard)) {
                 option = 1;
-            } else option = 2;
+            } else
+                option = generator.nextInt(2) + 2;
         } else if (deck.getSizeOfPile() < 3) {
             option = 1;
-        }  else {
+        } else {
             double usedCards = (double) opponent.numOfPutCards / opponent.startHandSize;
             System.out.println(opponent.numOfPutCards);
-            System.out.println(opponent.startHandSize );
+            System.out.println(opponent.startHandSize);
             System.out.println(usedCards);
             double chance = getChanceForCheck(usedCards);
             System.out.println(chance);
@@ -59,12 +65,13 @@ public class Computer extends Player {
 
     private boolean haveValidCard(Card patternCard) {
 
-        for(Card card : cardsInHand) {
-            if (card.isSameColor(patternCard) || card.isSameRank(patternCard) ) {
+        for (Card card : cardsInHand) {
+            if (card.isSameColor(patternCard) || card.isSameRank(patternCard)) {
                 validCard = card;
-                return true;
-            } 
-        } return false;
+                validCards.add(card);
+            }
+        }
+        return validCards.size() > 0;
     }
 
     private int randomChoice(double percent) {
@@ -73,13 +80,14 @@ public class Computer extends Player {
 
         if (randomInt < percent) {
             return 2;
-        } else return 1;
+        } else
+            return 1;
 
     }
 
     private double getChanceForCheck(double percent) {
-        double base = (double) 2/3;
-        if (percent < (double)1/3) {
+        double base = (double) 2 / 3;
+        if (percent < (double) 1 / 3) {
             return percent * 2;
         }
         return 540 * Math.pow((percent - base), 2) + 20;
