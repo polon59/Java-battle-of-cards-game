5e6f1e57a19ea7ed930eb283e9b3414707dc4633
polon59 <przemyslaw.polczak@gmail.com>
2018-04-27 00:08:17 +0200

    implement setter for 'playerName' variable from abstract class 'Player'

--- Person.java
+++ Person.java
@@ -10,6 +10,7 @@ public class Person extends Player{
         drawCards(deck);
         sortCards();
         this.startHandSize = cardsInHand.size();
+        this.playerName = "person";
     }
     
 
